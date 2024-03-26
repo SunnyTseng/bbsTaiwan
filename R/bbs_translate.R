ffad2e5117ea920a93fa80ffb5b5c6edd0b1588b
Sunny Tseng <sunnyyctseng@gmail.com>
2024-03-25 20:43:52 -0700

    created the first function

---
+++ R/bbs_translate.R
@@ -0,0 +1,12 @@
+#' Find species' English common name or scientific name
+#'
+#' @param species A character of the bird species' name in Chinese
+#'
+#' @return A character of the bird species' name in English
+#' @export
+#'
+#' @examples
+#' bbs_translate(species = "烏頭翁")
+bbs_translate <- function(species) {
+  print(species)
+}
