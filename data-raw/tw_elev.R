dfc0c1556843001b2028ddc7be244bab60743058
Sunny Tseng <sunnyyctseng@gmail.com>
2024-04-30 11:38:58 -0700

    Got rid of the raster package and used terra

--- data-raw/tw_elev.R
+++ data-raw/tw_elev.R
@@ -2,10 +2,8 @@
 
 # From: https://github.com/WanJyunChen/Taiwan_environmental_dataset
 
-tw_elev <- raster::raster(here::here("data-raw", "G1km_TWD97-121_DTM_ELE.tif")) |>
-  raster::projectRaster(crs = "epsg:4326") |>
-  raster::crop(c(xmin = 119.1, xmax = 122.1,
-                 ymin = 21.75, ymax = 25.35))
-
+tw_elev <- terra::rast(here::here("data-raw", "G1km_TWD97-121_DTM_ELE.tif")) |>
+  terra::project(y = "epsg:4326") |>
+  terra::crop(y = terra::ext(119.1, 122.1, 21.75, 25.35))
 
 usethis::use_data(tw_elev, overwrite = TRUE)
