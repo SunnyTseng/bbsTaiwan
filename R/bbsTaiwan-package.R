05ae0e6f146f685c75362ecf84dc697e34a17859
Sunny Tseng <sunnyyctseng@gmail.com>
2024-06-02 20:20:19 -0700

    fixed the namespace issure

--- R/bbsTaiwan-package.R
+++ R/bbsTaiwan-package.R
@@ -4,3 +4,17 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+# Dealing with CRAN Notes due to Non-standard evaluation
+
+utils::globalVariables(
+  # Vars used in Non-Standard Evaluations, declare here to
+  # avoid CRAN warnings
+  c("East", "Mountain", "North", "West", "bird_info", "chineseName", "day", "decimalLatitude",
+    "decimalLongitude", "distance", "elev", "event", "eventID",
+    "extendedmeasurementorfact", "flock", "habitat", "id", "in_out", "individualCount",
+    "locality", "locationID", "measurementDeterminedDate", "measurementType",
+    "measurementValue", "measurementorfacts", "month", "n", "n_sites", "occurrence",
+    "occurrenceID", "region", "scientificName", "site", "site_1", "site_n", "time_slot", "trip",
+    "tw_elev", "tw_map", "tw_region", "type", "vernacularName", "weather", "wind", "year", "zone")
+  )
